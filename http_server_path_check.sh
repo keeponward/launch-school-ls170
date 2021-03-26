bbd94cf4fb82c2c5eaa340e25a9c32eb55547208
Andy Witek <keeponward@gmail.com>
2021-03-26 10:57:11 -0700

    LS170 Networking Foundations

---
+++ http_server_path_check.sh
@@ -0,0 +1,26 @@
+#!usr/local/bin/bash
+
+function server () {
+  while true
+  do
+    read method path version
+    if [[ $method = 'GET' ]]
+    then
+      echo $path
+      if [[ -f "./www/$path" ]]
+      then
+        echo -ne 'HTTP/1.1 200 OK\r\n\r\n'; cat "./www/$path"
+      else
+        echo -ne 'HTTP/1.1 404 Not Found\r\n\r\n'
+      fi
+    else
+      echo -ne 'HTTP/1.1 400 Bad Request\r\n\r\n'
+    fi
+  done
+}
+
+coproc SERVER_PROCESS { server; }
+
+netcat -lvp 2345 <&${SERVER_PROCESS[0]} >&${SERVER_PROCESS[1]}
+
+
